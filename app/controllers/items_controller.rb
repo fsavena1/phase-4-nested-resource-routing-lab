95381d3a1f51bbc7d8b345663985504ad3483f85
frank <franksavena@gmail.com>
2023-01-14 12:13:50 -0600

    commit tree d23ecea8dd5a65c7d092fbef9331dab49b17675e

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,47 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
+    render json: items, include: :user 
   end
+
+
+  def show
+    if params[:user_id]
+       user = User.find(params[:user_id])
+    else 
+      item = Item.find(params[:id])
+    end
+    render json: item
+  end
+
+  def create 
+    user = User.find(params[:user_id])
+    item = user.Item.create(params[:name, :description, :price])
+    render json: item
+  end 
+
+
+
+
+
+
+
+
+
+
+
+
+  # def show 
+  #   item = Item.find(params[:item_id])
+  #   render json: item 
+  # end 
+
+
 
 end
